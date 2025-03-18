3f1e2c0270a683f80e080d88f957f53e09742480
Lexy Mangkubumi <lexysaputra25@gmail.com>
2025-03-18 21:33:02 +0700

    feat: adding table tryout

--- config/database/table/controler.js
+++ config/database/table/controler.js
@@ -2,5 +2,7 @@ const teacher = require('./teacherTable')
 const admin = require('./adminTable')
 const information = require('./informationTable')
 const subjectCategory = require('./subjectCategoryTable')
+const tryout = require('./tryoutTable')
+const tryout = require('./tryoutTable')
 
-module.exports = { teacher, admin, information, subjectCategory }
+module.exports = { teacher, admin, information, subjectCategory, tryout }
