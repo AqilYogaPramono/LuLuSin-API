123ffe52e07e70268a26f00a46a4f030b4f1a34a
Mawla <agielsingo@gmail.com>
2025-03-19 13:26:52 +0700

    feat: adding table studentsanswer

--- config/database/table/controler.js
+++ config/database/table/controler.js
@@ -1,8 +1,9 @@
 const teacher = require('./teacherTable')
 const admin = require('./adminTable')
 const information = require('./informationTable')
-const subjectCategory = require('./subjectCategoryTable')
+const subjectcategory = require('./subjectCategoryTable')
 const tryout = require('./tryoutTable')
 const scoretryout = require('./scoreTryoutTable')
+const studentsanswer = require('./studentsAnswersTable')
 
-module.exports = { teacher, admin, information, subjectCategory, tryout, scoretryout }
+module.exports = { teacher, admin, information, subjectcategory, tryout, scoretryout, studentsanswer }
