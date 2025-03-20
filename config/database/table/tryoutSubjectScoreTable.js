73f8c1af5fcef2cd8f6e8ae8c2d38c480d7ce730
Mawla <agielsingo@gmail.com>
2025-03-20 09:55:15 +0700

    feat: adding table tryout subject score

--- config/database/table/tryoutSubjectScoreTable.js
+++ config/database/table/tryoutSubjectScoreTable.js
@@ -1,1 +1,17 @@
-//mawla
+const {DataTypes} = require(`sequelize`)
+const {db} = require(`../connection`)
+
+const tryoutSubjectScoreTable = db.define(`tryoutsubjectscore`,{
+    tryout_subject_score_id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    id_student_answer: DataTypes.INTEGER,
+    average_score: DataTypes.INTEGER,
+    totally_correct: DataTypes.INTEGER,
+    totally_wrong: DataTypes.INTEGER,
+    totally_empty: DataTypes.INTEGER
+})
+
+module.exports = tryoutSubjectScoreTable
