49e2752b4ae57e50e3fc7a144ad720e101a2a7c1
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-03-30 12:06:29 +0700

    feat: add auth to login and fix connection to database

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,7 +1,17 @@
-const connection = require('../config/database')
+const { db } = require('../config/database/connection');
 
 class adminModel {
-
+    static async login(email) {
+        try {
+            const [rows] = await db.query("select * from admins where email = ?", {replacements: [email]})
+            if (rows.length == 0 ) {
+                return null
+            }
+            return rows[0]
+        } catch (err) {
+            throw err
+        }
+    }
 }
 
 module.exports = adminModel
