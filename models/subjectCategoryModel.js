4ff0f797cd92514704d35c19fb6905615b6d8725
Aqil Yoga Pramono <aqilyogapra1@gmail.com>
2025-03-30 22:30:05 +0700

    Update subjectCategoryModel.js

--- models/subjectCategoryModel.js
+++ models/subjectCategoryModel.js
@@ -3,7 +3,7 @@ const { db } = require('../config/database/connection');
 class subjectCategoryModel {
     static async getall() {
         try {
-            const [rows] = await db.query("select subject_category_id, subject_category_name from subject_categories")
+            const [rows] = await db.query("select * from subject_categories")
             return rows
         } catch (err) {
             throw err
