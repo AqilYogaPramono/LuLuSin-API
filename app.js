d2bd954eee3687b934a6b010c23c12e16390a2cb
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-03-30 14:13:46 +0700

    feat: adding store tryoutname

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.listen(port, () => {
 
 async function initialize() {
     await connectDB()
-    await db.sync( { alter: true }) //force: true
+    await db.sync( { alter: false, force: false })
     console.log("Database success sync")
 }
 
