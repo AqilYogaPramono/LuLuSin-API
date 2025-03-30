62a24aceed1b7eb4f1f4bf5415a16ffd2da5c84a
Mawla <agielsingo@gmail.com>
2025-03-30 22:25:28 +0700

    feat: categorysubject getall

--- models/subjectCategoryModel.js
+++ models/subjectCategoryModel.js
@@ -1,7 +1,15 @@
 const { db } = require('../config/database/connection');
 
 class subjectCategoryModel {
-
+    static async getall() {
+        try {
+            const [rows] = await db.query("select subject_category_id, subject_category_name from subject_categories")
+            return rows
+        } catch (err) {
+            throw err
+        }
+    }
+    
 }
 
 module.exports = subjectCategoryModel
