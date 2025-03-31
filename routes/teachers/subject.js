88f4033eb70e38b235e11ab69d1bd1cb00f4bc24
Mawla <agielsingo@gmail.com>
2025-03-31 17:20:29 +0700

    feat: feat subject getall

--- routes/teachers/subject.js
+++ routes/teachers/subject.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const subject = require('../../models/subjectModel')
 
 //get tabel subjek
+router.get('/teacher/subject', async (req, res, next) => {
+    try {
+        let datasubject = await subject.getall()
+        res.status(200).json({ datasubject })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
 //post ketgori subjek dan subjek
+
+
 //patch ketegori subjek dan subjek
 
+
+// delete subjek
 
 module.exports = router;
