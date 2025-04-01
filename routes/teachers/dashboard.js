cc0d7eef84c6ce2dac722ed8f452be140a04eff0
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-04-01 23:28:08 +0700

    fix: change output and tidy up the code

--- routes/teachers/dashboard.js
+++ routes/teachers/dashboard.js
@@ -4,17 +4,12 @@ const tryout = require('../../models/tryoutModel')
 
 // menampilkan nama tryout, total soal dibuat, hide or show
 router.get('/teacher/dashboard', async (req, res, next) => { 
-    let status = req.query.status
-
     try {
-        let tryouts = await tryout.Tdashboard(status)
-        res.status(200).json({ data: tryouts })
+        let tryouts = await tryout.dashboard()
+        res.status(200).json({ tryouts })
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 })
-
-//menampilkan nama guru yang login
-//menampilkan tryout yang belum di publis
 
 module.exports = router;
