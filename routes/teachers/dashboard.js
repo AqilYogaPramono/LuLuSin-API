64f1377530bb7c195014b043690605ce7bbc9c07
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-04-02 05:00:08 +0700

    feat: adding show data teacher name when tacher login

--- routes/teachers/dashboard.js
+++ routes/teachers/dashboard.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const tryout = require('../../models/tryoutModel')
-const { verifyToken, authorize } = require('../../config/middleware/jwt')
+const teacher = require('../../models/teacherModel')
+const { verifyToken, authorize } = require('../../config/middleware/jwt');
 
 // menampilkan nama tryout, total soal dibuat demhan status hide
 router.get('/teacher/dashboard', verifyToken, authorize(['teacher']),async (req, res, next) => { 
     try {
+        let teacherId = req.user.id
+        let teacherData = await teacher.getUsername(teacherId)
         let tryouts = await tryout.dashboard()
-        res.status(200).json({ tryouts })
+        res.status(200).json({ teacherData,tryouts })
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
