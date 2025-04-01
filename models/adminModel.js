f4bf28c5b3b7ced6c70445927f2a75bee44e3982
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-04-02 04:44:43 +0700

    feat: adding get admin name when admin login

--- models/adminModel.js
+++ models/adminModel.js
@@ -13,9 +13,18 @@ class adminModel {
         }
     }
 
-    static async getTotalUsers(email) {
+    static async getTotalUsers() {
         try {
-            const [rows] = await db.query("select (select count(student_id) from students) as total_students, (select count(teacher_id) from teachers) total_teachers", {replacements: [email]})
+            const [rows] = await db.query("select (select count(student_id) from students) as total_students, (select count(teacher_id) from teachers) total_teachers")
+            return rows[0]
+        } catch (err) {
+            throw err
+        }
+    }
+
+    static async getAdmin(adminId) {
+        try {
+            const [rows] = await db.query("select admin_name from admins where admin_id = ?", { replacements: [adminId] })
             return rows[0]
         } catch (err) {
             throw err
