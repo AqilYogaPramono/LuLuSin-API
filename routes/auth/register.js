b360b01f7a4e2051b4ba72b9e2bd0a9aa3ba5faf
Mawla <agielsingo@gmail.com>
2025-04-01 09:30:47 +0700

    feat: feat auth register

--- routes/auth/register.js
+++ routes/auth/register.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const student = require('../../models/studentModel')
 
 //post untuk melakuakn register siswa
+router.post('/register', async (req, res, next) => {
+    let { NISN, student_name, email, password } = req.body
+    let data = { NISN, student_name, email, password }
+
+    try {
+        await student.register(data)
+        res.status(201).json({ message: 'CREATED' })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
 module.exports = router;
