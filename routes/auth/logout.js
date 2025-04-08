ea4313f43e755de00d739a422441de62af34027d
Lexy Mangkubumi <lexysaputra25@gmail.com>
2025-04-08 23:01:15 +0700

    fix: auth logout

--- routes/auth/logout.js
+++ routes/auth/logout.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 //post untuk melakukan logout
-router.post('/logout', async (req, res, next) => {
-    try {
-        
-    } catch (error) {
-        res.status(500).json({ message: error.message })
+router.post('/logout', (req, res) => {
+    const token = req.headers.authorization?.split(' ')[1] 
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided.' })
     }
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Token Invalid' })
+        }
+
+        res.status(200).json({ message: 'Logout successful.' })
+    })
 })
 
 module.exports = router;
