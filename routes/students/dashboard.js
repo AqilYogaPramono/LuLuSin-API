5be3b13ea892e567a73ff6d098a833f0782f09af
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-05-06 16:57:41 +0700

    fix: counthdown scrapping

--- routes/students/dashboard.js
+++ routes/students/dashboard.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 const studentModel = require('../../models/studentModel')
 const { verifyToken, authorize } = require('../../config/middleware/jwt')
+const { countdown } = require('../services/scraperService')
 
 //menampilkan countdown snbt di mulai
 //menampilkan top 3 nilai nilai ter tinggi untuk siswa yang login
@@ -16,5 +17,18 @@ router.get('/student/dashboard', verifyToken, authorize (['student']), async (re
         res.status(500).json ({ message: error.message })
     }
 })
+
+// router.get('/student/dashboard', verifyToken, authorize (['student']), async (req, res, next) => {
+//     try {
+//         let idStudent = req.user.id
+        
+//         const notDoneTryouts = await studentModel.getNotDoneTryouts(idStudent)
+//         const countdownSNBT = await countdown()
+
+//         res.status(200).json ({ notDoneTryouts,  countdownSNBT})
+//     } catch (error) {
+//         res.status(500).json ({ message: error.message })
+//     }
+// })
 
 module.exports = router
