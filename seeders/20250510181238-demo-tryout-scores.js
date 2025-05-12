6289c0a98ed7aca0412135c2e1d12c02e8282567
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-05-13 04:08:54 +0700

    fix: seed tryout n subjek score

--- seeders/20250510181238-demo-tryout-scores.js
+++ seeders/20250510181238-demo-tryout-scores.js
@@ -3,23 +3,15 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('tryout_scores', [
+      { id_tryout: 2, id_student: 1, average_score: 54, total_correct: 9, total_wrong: 7, total_empty: 0 },
+      { id_tryout: 2, id_student: 2, average_score: 100, total_correct: 16, total_wrong: 0, total_empty: 0 },
+      { id_tryout: 1, id_student: 1, average_score: 100, total_correct: 16, total_wrong: 0, total_empty: 0 },
+      { id_tryout: 1, id_student: 2, average_score: 54, total_correct: 9, total_wrong: 7, total_empty: 0 },
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+
   }
 };
