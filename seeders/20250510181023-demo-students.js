2e92d887498fc6c87d5b021d0086a47315b25d45
Mawlaz01 <agielsingo@gmail.com>
2025-06-08 18:36:34 +0700

    feat: db status

--- seeders/20250510181023-demo-students.js
+++ seeders/20250510181023-demo-students.js
@@ -11,19 +11,22 @@ module.exports = {
         student_name: 'Ifzal',
         NISN: '123456',
         email: 'ifzal@students.com',
-        password: '$2b$10$V3.v67xgl6LczjheIv2TE.Xqky00g0ehU7XvAxjCx.GEa6WZpMEPO'
+        password: '$2b$10$V3.v67xgl6LczjheIv2TE.Xqky00g0ehU7XvAxjCx.GEa6WZpMEPO',
+        status: 'accept'
       },
       {
         student_name: 'Dafa',
         NISN: '234561',
         email: 'dafa@students.com',
-        password: '$2b$10$4CjxpwwcXn9qNu4SMSJx4OzGOMwrwNQSs19S29KeKvCD/uLwLrhEO'
+        password: '$2b$10$4CjxpwwcXn9qNu4SMSJx4OzGOMwrwNQSs19S29KeKvCD/uLwLrhEO',
+        status: 'accept'
       },
       {
         student_name: 'Haidar',
         NISN: '345612',
         email: 'haidar@students.com',
-        password: '$2b$10$LX/vwa5nLWvOdFJG.Afooe4earopaGRcY2z30PoJ5g3m623PvLXHS'
+        password: '$2b$10$LX/vwa5nLWvOdFJG.Afooe4earopaGRcY2z30PoJ5g3m623PvLXHS',
+        status: 'accept'
       }
     ], {});
   },
